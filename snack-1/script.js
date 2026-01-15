865164e4de0a0e6ebae04f95b4e01d0d8699b01f
nicolosalvestrini <nicolo.salvestrini@gmail.com>
2026-01-15 13:42:22 +0100

    first commit

---
+++ snack-1/script.js
@@ -0,0 +1,20 @@
+const prodotti = ['Latte', 'Pane', 'Uova'];
+const lista = document.getElementById('lista');
+const bottone = document.getElementById('aggiungi');
+
+for (let i = 0; i < prodotti.length; i++) {
+  const li = document.createElement('li');
+  li.textContent = prodotti[i];
+  lista.appendChild(li);
+}
+
+bottone.addEventListener('click', function() {
+  const nuoviProdotti = ['Pasta', 'Riso', 'Pomodori'];
+  const random = Math.floor(Math.random() * nuoviProdotti.length);
+  const li = document.createElement('li');
+  li.textContent = nuoviProdotti[random];
+  lista.appendChild(li);
+  prodotti[5] = nuoviProdotti[random];
+  prodotti.push(random)
+  console.log('Lunghezza array:', prodotti.length);
+});
