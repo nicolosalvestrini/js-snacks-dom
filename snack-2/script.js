321316b5798bbbaec9730447b74fb6f00d916d73
nicolosalvestrini <nicolo.salvestrini@gmail.com>
2026-01-15 14:02:24 +0100

    debug form login

--- snack-2/script.js
+++ snack-2/script.js
@@ -5,5 +5,10 @@ form.addEventListener('submit', function(event) {
   event.preventDefault()
   const username = document.getElementById('username').value;
   const email = document.getElementById('email').value;
+  if(username === "" || email === ""){
+    messaggio.textContent = "hai lasciato un parametro vuoto"
+  } else{
+    messaggio.textContent = "hai inserito i parametri corretti"
+  }
 
 });
