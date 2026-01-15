69806e724640fb5bd2eaaf2057eee60a092e8b16
nicolosalvestrini <nicolo.salvestrini@gmail.com>
2026-01-15 13:56:33 +0100

    sdd snack-2 folder

---
+++ snack-2/script.js
@@ -0,0 +1,9 @@
+const form = document.getElementById('registrazione');
+const messaggio = document.getElementById('messaggio');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault()
+  const username = document.getElementById('username').value;
+  const email = document.getElementById('email').value;
+
+});
